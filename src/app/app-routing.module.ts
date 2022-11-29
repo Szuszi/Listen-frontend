b632ef326a7492a7189b6215d63b049e3752158e
Álmos Koncz <61591712+Szuszi@users.noreply.github.com>
2022-11-29 22:42:31 +0100

    Add routing base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'newest', component: AppComponent },
+  { path: 'upload', component: AppComponent },
+  { path: 'user/:id/edit', component: AppComponent },
+  { path: 'favorites', component: AppComponent },
+  { path: 'user/:id', component: AppComponent },
+  { path: 'user-track/:id', component: AppComponent },
+  { path: '', redirectTo: '/newest', pathMatch: "full"},
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
