76fcb4f1bc67939bcef21d6cdd5b9d10a5e947dc
Álmos Koncz <61591712+Szuszi@users.noreply.github.com>
2022-12-05 22:40:13 +0100

    Add base of user page

--- src/app/pages/user-page/user-page.component.ts
+++ src/app/pages/user-page/user-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { mockUsers, mockUserTracks } from 'src/app/model/mock-data';
+import { UserTrack } from 'src/app/model/user-track.model';
+import { User } from 'src/app/model/user.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-page',
@@ -6,7 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-page.component.css'],
 })
 export class UserPageComponent implements OnInit {
-  constructor() {}
+  user: User | undefined;
+  tracks: UserTrack[] | undefined;
 
-  ngOnInit(): void {}
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.loadUserData();
+    console.log('User loaded');
+  }
+
+  private loadUserData() {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id) {
+      const idNumber = Number(id);
+
+      const foundUser = mockUsers.find(user => user.id === idNumber);
+      const foundTracks = mockUserTracks.filter(
+        track => track.userId === idNumber
+      );
+
+      console.log(foundUser);
+
+      if (foundUser) this.user = foundUser;
+      if (foundTracks) this.tracks = foundTracks;
+    }
+  }
 }
